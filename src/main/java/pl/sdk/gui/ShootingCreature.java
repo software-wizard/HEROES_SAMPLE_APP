3dcae6729687b4c139c9c378f1e70038c8cc6024
Software-Wizard <wieczorek.przemyslaw@outlook.com>
2020-08-26 21:00:32 +0200

    x

--- src/main/java/pl/sdk/gui/ShootingCreature.java
+++ src/main/java/pl/sdk/gui/ShootingCreature.java
@@ -5,4 +5,9 @@ public class ShootingCreature extends Creature {
     public ShootingCreature(int aMaxHp, Integer aAttack, Integer aArmor, String aName, int aMoveRange, int aAmount) {
         super(aMaxHp, aAttack, aArmor, aName, aMoveRange, aAmount);
     }
+
+    @Override
+    protected void ca(Creature aDefender) {
+        
+    }
 }
