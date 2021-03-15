dd881e87399a6025f22de8af703b4be9aaef43dc
Software-Wizard <przemek@szkola-dobrego-kodu.pl>
2021-03-15 15:58:56 +0100

    test for disapearing lava

--- src/test/java/pl/sdk/gui/gui/MovingTest.java
+++ src/test/java/pl/sdk/gui/gui/MovingTest.java
@@ -1,6 +1,7 @@
 package pl.sdk.gui.gui;
 
 import org.junit.jupiter.api.Test;
+import pl.sdk.gui.Creature;
 import pl.sdk.gui.CreatureFactory;
 
 import java.awt.*;
@@ -37,5 +38,41 @@ class MovingTest {
 
         //then
         assertFalse(result);
+    }
+
+    @Test
+    void creatureShouldCanStandOnLava(){
+        //given
+        HashMap<Point, GuiTileIf> testMap = new HashMap<>();
+        testMap.put(new Point (0,1), CreatureFactory.create(CreatureFactory.WATER_ELEMENTAL));
+        testMap.put(new Point (1,1), new LavaObstacle());
+        GameEngine engine = new GameEngine(testMap);
+
+        //when
+        boolean result = engine.isMoveAllowed(1, 1);
+
+        //then
+        assertTrue(result);
+    }
+
+    @Test
+    void LavaShouldNotDisappearAfterCreatureMove(){
+        //given
+        HashMap<Point, GuiTileIf> testMap = new HashMap<>();
+        Creature creature = CreatureFactory.create(CreatureFactory.WATER_ELEMENTAL);
+        testMap.put(new Point (0,1), creature);
+        LavaObstacle lava = new LavaObstacle();
+        testMap.put(new Point (1,1), lava);
+        GameEngine engine = new GameEngine(testMap);
+
+        //when
+        engine.move(1, 1);
+        GuiTileIf tempResult = engine.board.get(new Point(1, 1));
+        engine.move(1, 2);
+        GuiTileIf result = engine.board.get(new Point(1, 1));
+
+        //then
+        assertEquals(tempResult, creature);
+        assertEquals(result, lava);
     }
 }
